5c1aebf17d95c61c71ef7543946da1e80d9eb71f
Yunsu4 <ysa020428@gmail.com>
2024-12-07 21:24:41 +0900

    feat: 프로그램 실행 기능 추가

--- src/main/java/oncall/Application.java
+++ src/main/java/oncall/Application.java
@@ -1,7 +1,20 @@
 package oncall;
 
+import oncall.Controller.DateController;
+import oncall.Controller.FrontController;
+import oncall.view.InputView;
+import oncall.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        DateController dateController = new DateController(inputView);
+
+        final FrontController frontController = new FrontController(
+                inputView, outputView, dateController);
+
+        frontController.runMatchingProgram();
+
     }
 }
